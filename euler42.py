f630f209c63d069eb3be88c001352785696567ad
edwu <edwardwu4@gmail.com>
2013-02-23 12:03:35 -0800

    removed hardcoded dictionary

--- euler42.py
+++ euler42.py
@@ -1,8 +1,5 @@
 #The goal of this solution was to be as fast as possible and not precompute any of the triangle numbers.
 #Therefore this solution should be able to handle any sized word. 
-letter_to_value = {'a':1,'b':2,'c':3,'d':4,'e':5,'f':6,'g':7,'h':8,'i':9,
-				'j':10,'k':11,'l':12,'m':13,'n':14,'o':15,'p':16,'q':17,
-				'r':18,'s':19,'t':20,'u':21,'v':22,'w':23,'x':24,'y':25,'z':26}
 #prepopulate it with the 1st triangle number
 triangle_numbers = [1]
 
@@ -30,7 +27,7 @@ def value_of_word(word):
 	return sum(value_of_letter(letter) for letter in word)
 	
 def value_of_letter(letter):
-	return letter_to_value[letter.lower()]
+	return ord(letter.lower()) - ord('a') + 1
 	
 def binary_search(input,low,high,sorted_list):
 	if low > high:
